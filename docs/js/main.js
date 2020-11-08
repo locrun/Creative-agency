a7831af8631615e1dfeb2fcdc8e2127c9cca4b83
Runar <github.runar@gmail.com>
2020-11-08 09:59:08 +0300

    image compression

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
     items: 1,
   });
 });
+
+const ratingItemsList = document.querySelectorAll(".rating__item");
+const ratingItemsArray = Array.from(ratingItemsList);
+
+ratingItemsArray.forEach((item) => {
+  item.addEventListener("click", () => {
+    const { ItemValue } = item.dataset;
+    item.parentNode.dataset.totalValue = item.dataset.itemValue;
+  });
+});
